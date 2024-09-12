70dd2c7a60cd800b80df2399c31bf0ad44a57cba
조영혜 <59547170+dudgp128@users.noreply.github.com>
2024-09-12 13:27:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 (function () {
-    const template = document.createElement('template') 
+    const template = document.createElement("template");
     template.innerHTML = `
         <style>
         </style>
         <div id="root" style="width: 100%; height: 100%;"> 
         Hello Custom Widget
         </div>
-    `
+    `;
     class Main extends HTMLElement {
-      constructor () {
-        super()
-    
-        this._shadowRoot = this.attachShadow({ mode: 'open' })
-        this._shadowRoot.appendChild(template.content.cloneNode(true)) 
-        this._root = this._shadowRoot.getElementById('root')
-      }
+        constructor() {
+            super();
+
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+            this._root = this._shadowRoot.getElementById("root");
+        }
+
+        onCustomWidgetResize(width, height) {
+            this.render();
+        }
+
+        onCustomWidgetResize(changedProps) {}
+        onCustomWidgetDestory() {}
+
+        render() {
+            this._root.textContent = `Hello Custom Widget client ${this.clientWidth}, clientHeight${this.clientHeight}`;
+        }
     }
 
-    customElements.define('com-sap-sac-exercise-jyh0128-main', Main)
-
-})()
+    customElements.define("com-sap-sac-exercise-jyh0128-main", Main);
+})();
