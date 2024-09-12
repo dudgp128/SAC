56a7d84f5c7371419edcfffaa0597859a1ecf329
조영혜 <59547170+dudgp128@users.noreply.github.com>
2024-09-12 14:14:40 +0900

    Update main.js
    example code

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 (function () {
-    const template = document.createElement("template");
+    const template = document.createElement('template')
     template.innerHTML = `
-        <style>
-        </style>
-        <div id="root" style="width: 100%; height: 100%;"> 
-        Hello Custom Widget
-        </div>
-    `;
+          <style>
+          </style>
+          <div id="root" style="width: 100%; height: 100%;">
+          Hello Custom Widget
+          </div>
+        `
     class Main extends HTMLElement {
-        constructor() {
-            super();
-
-            this._shadowRoot = this.attachShadow({ mode: "open" });
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-            this._root = this._shadowRoot.getElementById("root");
-            console.log("constuctor");
-        }
-
-        onCustomWidgetResize(width, height) {
-            this.render();
-        }
-
-        onCustomWidgetAfterUdpate(changedProps) {
-            this.render();
-        }
-        onCustomWidgetDestory() {}
-
-        async render() {
-            const dataBinding=this.dataBinding;
-            if(!dataBinding || dataBinding.state !=='success'){
-                return;
-            }
-            this._root.textContent(JSON.stringify(dataBinding));
+      constructor () {
+        super()
+  
+        this._shadowRoot = this.attachShadow({ mode: 'open' })
+        this._shadowRoot.appendChild(template.content.cloneNode(true))
+  
+        this._root = this._shadowRoot.getElementById('root')
+      }
+  
+      onCustomWidgetResize (width, height) {
+        this.render()
+      }
+  
+      onCustomWidgetAfterUpdate (changedProps) {
+        this.render()
+      }
+  
+      onCustomWidgetDestroy () {
+      }
+        async render () {
+          const dataBinding = this.dataBinding
+          if (!dataBinding || dataBinding.state !== 'success') {
+             return
+           }
+           this._root.textContent = JSON.stringify(dataBinding)
         }
     }
-
-    customElements.define("com-sap-sac-exercise-jyh0128-main", Main);
-})();
+  
+    customElements.define('com-sap-sac-exercise-jyh0128-main', Main)
+  })()
+  
